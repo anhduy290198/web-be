4a596e2fa3f01ab4c2bf4b1b035a1aba79946259
duy <duyzin290198@gmail.com>
2024-05-22 08:08:52 +0700

    update db

--- laravel.sql
+++ laravel.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : web-be
+ Source Server         : web
  Source Server Type    : MySQL
- Source Server Version : 50173
- Source Host           : 127.0.0.1:3306
- Source Schema         : laravel
+ Source Server Version : 50733 (5.7.33-log)
+ Source Host           : localhost:3306
+ Source Schema         : laverel
 
  Target Server Type    : MySQL
- Target Server Version : 50173
+ Target Server Version : 50733 (5.7.33-log)
  File Encoding         : 65001
 
- Date: 18/05/2024 10:15:08
+ Date: 22/05/2024 08:08:17
 */
 
 SET NAMES utf8mb4;
@@ -25,7 +25,7 @@ CREATE TABLE `category`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of category
@@ -50,23 +50,28 @@ CREATE TABLE `product`  (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of product
 -- ----------------------------
-INSERT INTO `product` VALUES (1, 1, 'Iphone 14 ProMax', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (2, 1, 'Iphone 14 Pro', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (3, 1, 'Iphone 14', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (4, 1, 'Iphone 13 ProMax', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (5, 1, 'Iphone 13 Pro', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (6, 1, 'Iphone 13', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (7, 1, 'Iphone 12ProMax', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (8, 1, 'Iphone 12Pro', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (9, 1, 'Iphone 12', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (10, 1, 'Iphone 11 ProMax', NULL, 30000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', 10, '2024-05-18 09:48:51', '0000-00-00 00:00:00');
-INSERT INTO `product` VALUES (11, 1, 'Iphone 10', NULL, 10000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', NULL, '2024-05-18 03:11:18', '2024-05-18 03:11:18');
-INSERT INTO `product` VALUES (12, 1, 'Iphone 10', NULL, 10000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', NULL, '2024-05-18 03:12:19', '2024-05-18 03:12:19');
-INSERT INTO `product` VALUES (13, 1, 'Iphone 10', NULL, 10000000, '[\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\",\"https:\\/\\/cdn.tgdd.vn\\/Products\\/Images\\/42\\/114115\\/iphone-x-64gb-bac-org.png\"]', NULL, '2024-05-18 03:12:48', '2024-05-18 03:12:48');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(2) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `permission` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'duy', '123', 'Vũ Anh Duy', 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
